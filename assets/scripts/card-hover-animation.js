86d69d4184b5389c43e42d3a07bc8c5ed466f053
jesskabasilio <jessica.limabasilio@gmail.com>
2024-05-30 19:03:16 -0300

    add eventos e transições nos cards da Aplicação com Javascript

---
+++ assets/scripts/card-hover-animation.js
@@ -0,0 +1,23 @@
+function handleMouseEnter() {
+    this.classList.add('s-card--hovered');
+  }
+
+  function handleMouseLeave() {
+    this.classList.remove('s-card--hovered');
+  }
+
+
+function addEventListnersToCards() {
+    const cardElements = document.getElementsByClassName('s-card');
+    console.log(cardElements); 
+
+    for (let index = 0; index < cardElements.length; index++) {
+        const card = cardElements[index];
+        card.addEventListener('mouseenter', handleMouseEnter);
+        card.addEventListener('mouseleave', handleMouseLeave);
+        
+      }
+    }
+
+
+document.addEventListener("DOMContentLoaded", addEventListnersToCards, false);
